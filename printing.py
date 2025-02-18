79cc410bb84a8ad804b1ab35f3afe33e9401815c
Raffiki777 <prmalatjhb024@student.wethinkcode.co.za>
2025-02-18 12:21:22 +0200

    printing

---
+++ printing.py
@@ -0,0 +1,5 @@
+print("I love pizza!")
+
+# Pay attention to the color change in python
+# Pay attention to the errors in the console output
+# Get help from developer communities such as Stack Overflow
