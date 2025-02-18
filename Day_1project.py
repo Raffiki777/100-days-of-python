a3fca717c2718212aec0d3577b667d4e4f12442c
Raffiki777 <prmalatjhb024@student.wethinkcode.co.za>
2025-02-18 23:27:55 +0200

    Day 1 complete

---
+++ Day_1project.py
@@ -0,0 +1,7 @@
+# Band Generator tool
+
+print("Hey rockstar! Welcome to the Band Generator tool!\n")
+user_input1 = input("Enter the name of the city you grew up in\n").capitalize()
+user_input2 = input("Enter the name of your pet\n").capitalize()
+
+print("Your Band name is " + user_input1 + ' ' + user_input2 + "!")
--- exercise_2.py
+++
@@ -1,6 +0,0 @@
-# Fix the code below
-
-print("Day - String Manipulation")
-print('String Concatenation is done with the "+" sign')
-print('eg. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
